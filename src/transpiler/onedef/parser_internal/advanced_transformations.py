26c18dfd445ca591fb79c2ee3008e5ae9cd69f56
Eric Reed <mrericcoolnessreed@gmail.com>
2021-05-29 21:43:14 -0400

    more work on parser's advanced transformation, regex matcher utility function and a few partial usages

--- src/transpiler/onedef/parser_internal/advanced_transformations.py
+++ src/transpiler/onedef/parser_internal/advanced_transformations.py
@@ -8,20 +8,50 @@ class AdvancedTransformer:
     @staticmethod
     def transform (basic_list: BasicList) -> AppDefinition:
         app_definition: AppDefinition = AppDefinition ()
+        def raise_out_of_context (_subitem: BasicItem, block_type: str): raise Exception (f"subitem {_subitem} seems to be out of context in a {block_type} block")
 
         for top_level_item in basic_list:
             print (f"item: {top_level_item}")
-            matches, result = AdvancedTransformer._match (BasicStatementItem, top_level_item, r"instance (?P<name>.+) is (?P<type_name>.+)")
-            if matches:
-                print (f"matches! result is {result}, name is {result.group ('name')}, type_name is {result.group ('type_name')}")
 
+            is_type_definition_block, result = AdvancedTransformer._match (BasicBlockItem, r"type (?P<name>.+)", top_level_item)
+            if is_type_definition_block:
+                type_definition_block: BasicBlockItem = top_level_item
+                for type_definition_block_subitem in type_definition_block.subitems:
+                    is_type_definition_fields_node, result = AdvancedTransformer._match (BasicBlockItem, r"fields", type_definition_block_subitem)
+                    if is_type_definition_fields_node:
+                        type_definition_fields_node = type_definition_block_subitem
+                        for field_definition in type_definition_fields_node.subitems:
+                            print (f"field definition: {field_definition}")
+                            # TODO more parsing of field definition
+                        continue
+
+                    is_type_definition_actions_node, result = AdvancedTransformer._match (BasicBlockItem, r"actions", type_definition_block_subitem)
+                    if is_type_definition_actions_node:
+                        type_definition_actions_node = type_definition_block_subitem
+                        for action_definition_block in type_definition_actions_node.subitems:
+                            print (f"action definition: {action_definition_block}")
+                            # TODO more parsing of action definition
+                        continue
+
+                    raise_out_of_context (type_definition_block_subitem, "type definition")
+                # TODO construction + population of field object
+                continue
+
+            is_instance_definition, result = AdvancedTransformer._match (BasicStatementItem, r"instance (?P<name>.+) is (?P<type_name>.+)", top_level_item)
+            if is_instance_definition:
+                instance_definition: BasicStatementItem = top_level_item
+                print (f"instance definition: {instance_definition}")
+                # TODO more parsing of instance definition
+                continue
+
+            raise_out_of_context (top_level_item, "top level")
         # TODO re-enable check once the advanced transformation can find and assign the main view
         # if app_definition.main_view is None: raise Exception ("No main view specified")
 
         return app_definition
     @staticmethod
-    def _match (req_type: Union [type (BasicStatementItem), type (BasicBlockItem)], item: BasicItem, pattern: Regex) -> \
+    def _match (_type: Union [type (BasicStatementItem), type (BasicBlockItem)], pattern: Regex, item: BasicItem) -> \
     tuple [bool, MatchResult]:
-        if not isinstance (item, req_type): return False, None
+        if not isinstance (item, _type): return False, None
         match_result = re.match (pattern, item.value)
         return match_result is not None, match_result
